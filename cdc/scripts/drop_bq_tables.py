f70eb814753c72ef55421f4818c056a381dfe00f
rdxz2 <richard.dharmawan@gmail.com>
2024-12-15 21:26:29 +0700

    [DROP BQ] use env

--- cdc/scripts/drop_bq_tables.py
+++ cdc/scripts/drop_bq_tables.py
@@ -1,7 +1,15 @@
-from google.cloud import bigquery
+import dotenv
+import os
 
-client = bigquery.Client.from_service_account_json('/home/ubuntu/repos/xz2/demo/secrets/xz2-demo-9fc7663fdfac.json')
+from google.cloud import bigquery
+from loguru import logger
+
+dotenv.load_dotenv()
+
+SA_FILENAME = os.environ['SA_FILENAME']
+
+client = bigquery.Client.from_service_account_json(os.path.join(os.path.pardir, SA_FILENAME))
 all_tables = client.list_tables('log__stream')
 for table in all_tables:
-    print(f'Dropping {table.table_id}')
+    logger.info(f'Dropping {table.table_id}')
     client.delete_table(table)
