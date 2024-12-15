dd2da01783541e6b1575dfde1b6ffff9ad6b80e3
rdxz2 <richard.dharmawan@gmail.com>
2024-12-15 08:11:22 +0700

    [UPLOADER] include all stream process in the loop

--- cdc/cdc_uploader.py
+++ cdc/cdc_uploader.py
@@ -165,48 +165,48 @@ class Uploader:
                 current_chunk_size = 0
                 list__proto_rows.append(types.ProtoRows())
 
-        parent = self.write_client.table_path(*bq_table_log_fqn.split('.'))
-        # write_stream = types.WriteStream()
-
-        # When creating the stream, choose the type. Use the PENDING type to wait
-        # until the stream is committed before it is visible. See:
-        # https://cloud.google.com/bigquery/docs/reference/storage/rpc/google.cloud.bigquery.storage.v1#google.cloud.bigquery.storage.v1.WriteStream.Type
-        # write_stream.type_ = types.WriteStream.Type.PENDING
-        # write_stream = self.write_client.create_write_stream(
-        #     parent=parent, write_stream=write_stream
-        # )
-        # stream_name = write_stream.name
-        stream_name = f'{parent}/_default'
-
-        # Create a template with fields needed for the first request.
-        request_template = types.AppendRowsRequest()
-
-        # The initial request must contain the stream name.
-        request_template.write_stream = stream_name
-
-        # So that BigQuery knows how to parse the serialized_rows, generate a
-        # protocol buffer representation of your message descriptor.
-        proto_schema = types.ProtoSchema()
-        proto_descriptor = descriptor_pb2.DescriptorProto()
-        pb2_class.DESCRIPTOR.CopyToProto(proto_descriptor)
-        proto_schema.proto_descriptor = proto_descriptor
-        proto_data = types.AppendRowsRequest.ProtoData()
-        proto_data.writer_schema = proto_schema
-        request_template.proto_rows = proto_data
-
-        # Some stream types support an unbounded number of requests. Construct an
-        # AppendRowsStream to send an arbitrary number of requests to a stream.
-        append_rows_stream = writer.AppendRowsStream(self.write_client, request_template)
-
-        # Set an offset to allow resuming this stream if the connection breaks.
-        # Keep track of which requests the server has acknowledged and resume the
-        # stream at the first non-acknowledged message. If the server has already
-        # processed a message with that offset, it will return an ALREADY_EXISTS
-        # error, which can be safely ignored.
-        #
-        # The first request must always have an offset of 0.
         for i, proto_rows in enumerate(list__proto_rows):
-            logger.debug(f'{bq_table_log_fqn}: sending chunk {i + 1}')
+            parent = self.write_client.table_path(*bq_table_log_fqn.split('.'))
+            # write_stream = types.WriteStream()
+
+            # When creating the stream, choose the type. Use the PENDING type to wait
+            # until the stream is committed before it is visible. See:
+            # https://cloud.google.com/bigquery/docs/reference/storage/rpc/google.cloud.bigquery.storage.v1#google.cloud.bigquery.storage.v1.WriteStream.Type
+            # write_stream.type_ = types.WriteStream.Type.PENDING
+            # write_stream = self.write_client.create_write_stream(
+            #     parent=parent, write_stream=write_stream
+            # )
+            # stream_name = write_stream.name
+            stream_name = f'{parent}/_default'
+
+            # Create a template with fields needed for the first request.
+            request_template = types.AppendRowsRequest()
+
+            # The initial request must contain the stream name.
+            request_template.write_stream = stream_name
+
+            # So that BigQuery knows how to parse the serialized_rows, generate a
+            # protocol buffer representation of your message descriptor.
+            proto_schema = types.ProtoSchema()
+            proto_descriptor = descriptor_pb2.DescriptorProto()
+            pb2_class.DESCRIPTOR.CopyToProto(proto_descriptor)
+            proto_schema.proto_descriptor = proto_descriptor
+            proto_data = types.AppendRowsRequest.ProtoData()
+            proto_data.writer_schema = proto_schema
+            request_template.proto_rows = proto_data
+
+            # Some stream types support an unbounded number of requests. Construct an
+            # AppendRowsStream to send an arbitrary number of requests to a stream.
+            append_rows_stream = writer.AppendRowsStream(self.write_client, request_template)
+
+            # Set an offset to allow resuming this stream if the connection breaks.
+            # Keep track of which requests the server has acknowledged and resume the
+            # stream at the first non-acknowledged message. If the server has already
+            # processed a message with that offset, it will return an ALREADY_EXISTS
+            # error, which can be safely ignored.
+            #
+            # The first request must always have an offset of 0.
+            logger.debug(f'{bq_table_log_fqn}: sending chunk {i + 1} ({proto_rows.serialized_rows.__len__()} rows)')
             request = types.AppendRowsRequest()
             # request.offset = 0
             proto_data = types.AppendRowsRequest.ProtoData()
@@ -216,19 +216,19 @@ class Uploader:
             response_future_1 = append_rows_stream.send(request)
             # response_future_1_result = response_future_1.result()
 
-        # Shutdown background threads and close the streaming connection.
-        append_rows_stream.close()
-        # self.append_rows_streams.pop(bq_table_log_fqn)
+            # Shutdown background threads and close the streaming connection.
+            append_rows_stream.close()
+            # self.append_rows_streams.pop(bq_table_log_fqn)
 
-        # # A PENDING type stream must be "finalized" before being committed. No new
-        # # records can be written to the stream after this method has been called.
-        # write_client.finalize_write_stream(name=stream_name)
+            # # A PENDING type stream must be "finalized" before being committed. No new
+            # # records can be written to the stream after this method has been called.
+            # write_client.finalize_write_stream(name=stream_name)
 
-        # # Commit the stream you created earlier.
-        # batch_commit_write_streams_request = types.BatchCommitWriteStreamsRequest()
-        # batch_commit_write_streams_request.parent = parent
-        # batch_commit_write_streams_request.write_streams = [stream_name]
-        # write_client.batch_commit_write_streams(batch_commit_write_streams_request)
+            # # Commit the stream you created earlier.
+            # batch_commit_write_streams_request = types.BatchCommitWriteStreamsRequest()
+            # batch_commit_write_streams_request.parent = parent
+            # batch_commit_write_streams_request.write_streams = [stream_name]
+            # write_client.batch_commit_write_streams(batch_commit_write_streams_request)
 
     def upload(self, pg_table_fqn, filenames: set[str]):
         """
@@ -325,7 +325,7 @@ if __name__ == '__main__':
             # Group by table
             grouped_filenames: dict[str, set[str]] = {}
             for filename in filenames:
-                pg_table_fqn = os.path.basename(filename).split('-')[1].removesuffix('.json')  # yyyyMMddhhmmss-pg_table_fqn.json
+                pg_table_fqn = os.path.basename(filename).split('-')[-1].removesuffix('.json')  # yyyyMMddhhmmssffffff-yyyyMMddhhmmssffffff-pg_table_fqn.json
                 if pg_table_fqn not in grouped_filenames:
                     grouped_filenames[pg_table_fqn] = set()
                 grouped_filenames[pg_table_fqn].add(filename)
