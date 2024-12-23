2c753da6fa496f737d8ebed0456f9124101b7ec6
rdxz2 <richard.dharmawan@gmail.com>
2024-12-23 08:32:59 +0700

    [SCRIPT] drop_bq_tables: use env var

--- cdc/drop_bq_tables.py
+++ cdc/drop_bq_tables.py
@@ -8,8 +8,12 @@ dotenv.load_dotenv()
 
 SA_FILENAME = os.environ['SA_FILENAME']
 
-client = bigquery.Client.from_service_account_json(os.path.join(os.path.pardir, SA_FILENAME))
-all_tables = client.list_tables('log__stream')
+CDC_DB_NAME = os.environ['CDC_DB_NAME']
+
+BQ_LOG_DATASET_PREFIX = os.environ['BQ_LOG_DATASET_PREFIX']
+
+client = bigquery.Client.from_service_account_json(SA_FILENAME)
+all_tables = client.list_tables(f'{BQ_LOG_DATASET_PREFIX}{CDC_DB_NAME}')
 for table in all_tables:
     logger.info(f'Dropping {table.table_id}')
     client.delete_table(table)
