948dc29b87ed056916f1dbcc2250a9589acf1c4f
rdxz2 <richard.dharmawan@gmail.com>
2025-01-10 21:18:59 +0700

    [PREFECT] set profile for service

--- prefect/run-server.sh
+++ prefect/run-server.sh
@@ -1,3 +1,4 @@
 source ~/venv/prefect/bin/activate
 
+prefect profile use ephemeral
 prefect server start --host 0.0.0.0
--- prefect/run-worker.sh
+++ prefect/run-worker.sh
@@ -1,3 +1,4 @@
 source ~/venv/prefect/bin/activate
 
+prefect profile use local
 prefect worker start --pool worker-pool-2
