540736626642f6dad84150cfff5a895929bfb495
rdxz2 <richard.dharmawan@gmail.com>
2025-01-14 19:05:20 +0700

    [GSHEET TO BQ] change var name

--- airflow/dags/gsheet_to_bq.py
+++ airflow/dags/gsheet_to_bq.py
@@ -4,8 +4,7 @@ from airflow.operators.python import PythonOperator
 from airflow.utils.task_group import TaskGroup
 from datetime import datetime
 
-from config.settings import GCS_BUCKET
-from config.gsheet_to_bq import TARGETS_DAILY_CC_USAGE
+from config.settings import GCS_BUCKET, GSHEET__TARGETS_DAILY_CC_USAGE
 from plugins.operators.bigquery import GCSToBigQueryOperator
 from plugins.operators.gsheet import GSheetToGCSOperator
 from scripts.gsheet_transformer import transform_daily_cc_usage
@@ -20,38 +19,36 @@ with DAG(
     end = EmptyOperator(task_id='end')
 
     tgs = []
-    for target in TARGETS_DAILY_CC_USAGE:
+    for target in GSHEET__TARGETS_DAILY_CC_USAGE:
         with TaskGroup(target['task_group_id']) as tg:
-            gsheet_to_gcs = GSheetToGCSOperator(
-                task_id=f'gsheet_to_gcs',
+            task_gsheet_to_gcs = GSheetToGCSOperator(
+                task_id='gsheet_to_gcs',
                 id=target['gsheet']['id'],
                 sheet=target['gsheet']['sheet'],
                 range=target['gsheet']['range'],
                 gcs_bucket=GCS_BUCKET,
             )
 
-            transform = PythonOperator(
-                task_id=f'transform',
+            task_transform = PythonOperator(
+                task_id='transform',
                 python_callable=transform_daily_cc_usage,
                 op_kwargs={
-                    'gcs_filename': "{{{{ task_instance.xcom_pull(task_ids='{}', key='gcs_filename') }}}}".format(gsheet_to_gcs.task_id),
+                    'gcs_filename': "{{{{ task_instance.xcom_pull(task_ids='{}', key='gcs_filename') }}}}".format(task_gsheet_to_gcs.task_id),
                     'cols': target['bq_table']['cols'],
                 },
                 provide_context=True,
             )
 
-            gcs_to_bq = GCSToBigQueryOperator(
-                task_id=f'gcs_to_bq',
+            task_gcs_to_bq = GCSToBigQueryOperator(
+                task_id='gcs_to_bq',
                 bq_table_fqn=target['bq_table']['fqn'],
                 cols=target['bq_table']['cols'],
                 partition_col=target['bq_table']['partition_col'],
                 cluster_cols=target['bq_table']['cluster_cols'],
                 load_strategy=target['bq_table']['load_strategy'],
-                xcom_task_id=gsheet_to_gcs.task_id,
+                xcom_task_id=task_gsheet_to_gcs.task_id,
             )
 
-            gsheet_to_gcs >> transform >> gcs_to_bq
+            task_gsheet_to_gcs >> task_transform >> task_gcs_to_bq
 
             tgs.append(tg)
-
-    start >> tgs >> end
