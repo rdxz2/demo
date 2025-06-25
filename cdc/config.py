c0054726d2065944bad83dbd04132756bd1c4e0e
rdxz2 <richard.dharmawan@gmail.com>
2025-06-25 20:18:52 +0700

    finalize docker build for cdc, use model post init

--- cdc/config.py
+++ cdc/config.py
@@ -14,10 +14,10 @@ class Settings(BaseSettings):
     STREAM_PUBLICATION_NAME: str
     STREAM_REPLICATION_SLOT_NAME: str
 
-    LOG_DIR: Optional[str] = f'logs/{STREAM_DB_NAME}'
+    LOG_DIR: Optional[str] = None  # Set on model_post_init
 
     # Configure streamer
-    STREAM_OUTPUT_DIR: Optional[str] = f'output/{STREAM_DB_NAME}/stream'
+    STREAM_OUTPUT_DIR: Optional[str] = None  # Set on model_post_init
     STREAM_NO_MESSAGE_REPORT_INTERVAL_S: Optional[int] = 60  # If no message received for this time, report it
     STREAM_DELAY_PRINT_INTERVAL_S: Optional[int] = 1
     STREAM_CONSUMER_QUEUE_MAX_SIZE: Optional[int] = 1000  # Number of max messages to be held, to limit streamer's memory usage
@@ -26,7 +26,8 @@ class Settings(BaseSettings):
     STREAM_FILEWRITER_ALL_FILE_MAX_OPENED_TIME_S: Optional[int] = 600  # If a file is opened for this time, close all files
     STREAM_FILEWRITER_NO_MESSAGE_WAIT_TIME_S: Optional[int] = 600  # If no message received for this time, close all files
 
-    UPLOAD_OUTPUT_DIR: Optional[str] = f'output/{STREAM_DB_NAME}/upload'
+    # Configure uploader
+    UPLOAD_OUTPUT_DIR: Optional[str] = None  # Set on model_post_init
     UPLOAD_SA_FILENAME: Optional[str] = 'sa.json'
     UPLOAD_BQ_PROJECT_ID: str
     UPLOAD_BQ_DATASET_LOCATION: str
@@ -36,14 +37,29 @@ class Settings(BaseSettings):
     UPLOAD_STREAM_CHUNK_SIZE_B: Optional[int] = 8000000
     UPLOAD_NO_FILE_REPORT_INTERVAL_S: Optional[int] = 600
 
+    # Configure merger
+    MERGER_OUTPUT_DIR: Optional[str] = None  # Set on model_post_init
+
     # # Configure PG to BQ
-    # PGTOBQ_OUTPUT_DIR: str = f'output/{NAME}/bqtobq'
+    # PGTOBQ_OUTPUT_DIR: str = f'outputs/{NAME}/bqtobq'
     # PGTOBQ_UPLOAD_QUEUE_MAX_SIZE = 10
     # PGTOBQ_GCS_BUCKET: str
     # PGTOBQ_GCS_BASE_PATH: str
 
     # Configure alerts
     DISCORD_WEBHOOK_URL: Optional[str] = None
+
+    def model_post_init(self, context):
+        if self.LOG_DIR is None:
+            self.LOG_DIR = f'logs/{self.STREAM_DB_NAME}'
+        if self.STREAM_OUTPUT_DIR is None:
+            self.STREAM_OUTPUT_DIR = f'outputs/{self.STREAM_DB_NAME}/stream'
+        if self.UPLOAD_OUTPUT_DIR is None:
+            self.UPLOAD_OUTPUT_DIR = f'outputs/{self.STREAM_DB_NAME}/upload'
+        if self.MERGER_OUTPUT_DIR is None:
+            self.MERGER_OUTPUT_DIR = f'outputs/{self.STREAM_DB_NAME}/merge'
+
+        return super().model_post_init(context)
 
     class Config:
         env_file = '.env'
